24f43934c53e7f8b95cb2d2fc2e0b095317fdc92
ahajiyev628 <66174474+ahajiyev628@users.noreply.github.com>
2022-08-15 22:24:49 +0400

    Update Family.java

--- Family.java
+++ Family.java
@@ -1,125 +1,128 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
+import java.util.Objects;
 
-public class Family extends Human {
-    private static DayOfWeek d;
-    private static AnimalSpecies species;
-    public Family(String name, String surname, int year, int iq, Pet pet, Human mother, Human father, String[][] schedule) {
-        super(name, surname, year, mother, father);
+public class Family {
+    private Human[] children = new Human[0];
+    private Pet pet;
+    private Human mother;
+    private Human father;
+
+    public Family(Pet pet, Human mother, Human father, Human[] children) {
+        this.pet = pet;
+        this.mother = mother;
+        this.father = father;
+        this.children = children;
     }
-    static Pet p = new Pet();
 
-    static ArrayList<Human> parents = new ArrayList<>();
-    static ArrayList<Human> children = new ArrayList<>();
-    static ArrayList<Pet> pets = new ArrayList<>();
+    public Human[] getChildren() {
+        return children;
+    }
 
-    public Family(String name, String surname, int year, int iq, Human mother, Human father, String[][] schedule) {
-        super();
+    public void setChildren(Human[] children) {
+        this.children = children;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+
+    public Human getMother() {
+        return mother;
+    }
+
+    public void setMother(Human mother) {
+        this.mother = mother;
+    }
+
+    public Human getFather() {
+        return father;
+    }
+
+    public void setFather(Human father) {
+        this.father = father;
     }
 
     public Family() {
-
     }
 
-    public static void main(String[] args){
-        Human mother = new Family();
-        Human father = new Family();
-        Human child = new Family();
-
-        mother.setName("Tito");
-        mother.setSurname("Karleone");
-        mother.setYear(1960);
-        mother.setIq(60);
-        mother.setPet(new Pet());
-        mother.setFather(new Human("Allahverdi", "Hajiyev"));
-        mother.setMother(new Human("Allahverdi", "Hajiyev"));
-        mother.setSchedule(new String[][]{{String.valueOf(d.Sunday)}, {Human.tasks[0]}});
-
-
-        father.setName("Vito");
-        father.setSurname("Karleone");
-        father.setYear(1970);
-        father.setIq(70);
-        father.setPet(new Pet());
-        father.setFather(new Human("Allahverdi", "Hajiyev"));
-        father.setMother(new Human("Allahverdi", "Hajiyev"));
-        father.setSchedule(new String[][]{{String.valueOf(d.Thursday)}, {Human.tasks[4]}});
-
-        child.setName("Michael");
-        child.setSurname("Karleone");
-        child.setYear(1990);
-        child.setIq(50);
-        child.setPet(new Pet());
-        child.setFather(new Human(father.getName(), father.getSurname()));
-        child.setMother(new Human(mother.getName(), mother.getSurname()));
-        child.setSchedule(new String[][]{{String.valueOf(d.Saturday)}, {Human.tasks[6]}});
-
-        parents.add(new Family(mother.getName(), mother.getSurname(), mother.getYear(), mother.getIq(), mother.getMother(),mother.getFather(), mother.getSchedule()));
-        parents.add(new Family(father.getName(), father.getSurname(), father.getYear(), father.getIq(), father.getMother(),father.getFather(), father.getSchedule()));
-        children.add(new Family(child.getName(), child.getSurname(), child.getYear(), child.getIq(), child.getMother(),child.getFather(), child.getSchedule()));
-
-        p.setAge(20);
-        p.setNickname("wimel");
-        p.setTrickLevel(80);
-        p.setSpecies(String.valueOf(species.DOG));
-
-        pets.add(new Pet(p.getSpecies(),p.getNickname(),p.getAge(),p.getTrickLevel(), new String[]{"eat", "drink", "sleep"}));
-
-        HashMap<Integer, ArrayList<Human>> family = new HashMap<>();
-        family.put(1, parents);
-        family.put(2, children);
-        // family.put(3, pets);
-
-        System.out.println("number of children in the family: " + children.size());
-        System.out.println("Add new child to the family");
-        addChild(new Human("","",0, new Human("",""), new Human("","")));
-        System.out.println("number of children in the family: " + children.size());
-
-        // System.out.println("number of family member: " + (parents.size() + children.size()));
-
-        countFamily(family);
-
-        System.out.println(child.toString());
-        System.out.println(father.toString());
-        System.out.println(mother.toString());
+    public Human[] deleteChild(Human child) {
+        int i = 0;
+        if (children.length > 1) {
+            Human[] newChildren = new Human[children.length - 1];
+            for (Human ch : children) {
+                if (!ch.equals(child)) {
+                    newChildren[i] = ch;
+                    i++;
+                }
+            }
+            this.children = newChildren;
+        }
+        return children;
     }
 
-    public static Human addChild(Human ch) {
-        ch.setName("Child name");
-        ch.setSurname("Child surname");
-        ch.setYear(2000);
-        ch.setIq(80);
-        ch.setPet(pets.get(0));
-        ch.setSchedule(new String[][]{{String.valueOf(d.Monday)}, {Human.tasks[1]}});
-
-        ch = new Family(ch.getName(),
-                        ch.getSurname(),
-                        ch.getYear(),
-                        ch.getIq(),
-                        ch.getPet(),
-                        new Human("Vito", "Karleone"),
-                        new Human("Tito", "Karleone"),
-                        ch.getSchedule());
-
-        children.add(ch);
-        System.out.println("Child was added");
-        System.out.println(children.get(1).toString());
-        return ch;
+    public Human[] addChild(Human child) {
+        Human[] newChildren = new Human[children.length + 1];
+        newChildren[children.length] = child;
+        for (int i = 0; i < children.length; i++) {
+            newChildren[i] = children[i];
+        }
+        this.children = newChildren;
+        return children;
     }
 
-    public static void deleteChild(int position) {
-        children.remove(position);
-        System.out.println(position + "th child was removed from family");
+    public Human[] deleteChild(int position) {
+        int j = 0;
+        if (position > 0 && position < children.length) {
+            Human[] newChildren = new Human[children.length - 1];
+            for (int i = 0; i < children.length; i++) {
+                if (position != i) {
+                    newChildren[j] = children[i];
+                    j++;
+                }
+            }
+            this.children = newChildren;
+        } else {
+            throw new IndexOutOfBoundsException("Please enter the correct index");
+        }
+        return children;
     }
 
-    public static void countFamily(Map<Integer, ArrayList<Human>> fm) {
-        System.out.println("number of family member: " + (fm.get(1).size() + fm.get(2).size()));
+    public void countFamily() {
+        System.out.printf("number of family member: %d", children.length + 2);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Family family = (Family) o;
+        return Objects.equals(pet, family.pet) && Objects.equals(mother, family.mother) && Objects.equals(father, family.father) && Arrays.equals(children, family.children);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(pet, mother, father);
+        result = 31 * result + Arrays.hashCode(children);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Family{" +
+                "pet=" + pet +
+                ", mother=" + mother +
+                ", father=" + father +
+                ", children=" + Arrays.toString(children) +
+                '}';
     }
 
     @Override
     protected void finalize() throws Throwable {
-        super.finalize();
-        System.out.println("finalize() method was here :)");
+        System.out.println("redefined finalize() method");
     }
+
 }
