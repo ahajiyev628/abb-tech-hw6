34698953abe1ee38e7e76877a9143a4d7deea6f3
ahajiyev628 <66174474+ahajiyev628@users.noreply.github.com>
2022-08-24 22:16:32 +0400

    Add files via upload

---
+++ TestUnit.java
@@ -0,0 +1,61 @@
+package homework6;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class TestUnit {
+    private static DayOfWeek d;
+    private Assertions Assert;
+
+    Family fmly = new Family();
+    Human father = new Human("Vito","Karleone");
+    Human mother = new Human("Jane", "Karloane");
+    Human child = new Human();
+    Human child1 = new Human("Allahverdi", "Hajiyev");
+    Human child2 = new Human("Allahverdi2", "Hajiyev2");
+    Human child3 = new Human("Allahverdi3", "Hajiyev3");
+    Pet p = new Pet();
+
+    @Test
+    public void firstTest() {
+        fmly.setMother(mother);
+        fmly.setFather(father);
+
+        child.setName("Michael");
+        child.setSurname("Karloene");
+        child.setYear(20);
+        child.setIq(51);
+        child.setSchedule(new String[][]{{String.valueOf(d.Monday)}, {Human.tasks[1]}});
+
+        p.setSpecies(String.valueOf(AnimalSpecies.DOG));
+        p.setAge(5);
+        p.setNickname("wimel");
+        p.setTrickLevel(75);
+        p.setHabits(new String[]{"eat", "play"});
+
+        fmly.setPet(p);
+        fmly.setChildren(new Human[]{new Human()});
+
+        fmly.countFamily();
+        System.out.println("");
+        fmly.addChild(child1);
+        fmly.countFamily();
+        System.out.println("");
+        fmly.addChild(child2);
+        fmly.countFamily();
+        System.out.println("");
+        fmly.addChild(child3);
+        fmly.countFamily();
+        System.out.println("");
+
+        fmly.deleteChild(child2);
+        fmly.countFamily();
+        System.out.println("");
+        fmly.deleteChild(1);
+        fmly.countFamily();
+        System.out.println("");
+
+        System.out.println(fmly.toString());
+        Assert.assertTrue(true);
+    }
+}
